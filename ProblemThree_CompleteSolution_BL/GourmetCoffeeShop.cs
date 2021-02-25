0668c546464bab31c8191062c5a9447b5334d183
99003601 <78849903+99003601@users.noreply.github.com>
2021-02-25 17:29:02 +0530

    Update GourmetCoffeeShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,35 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetCoffeeShop : GourmetShop, ITaxCompute
     {
+        public double totalBillValue { get; set; }
+
+
+        public GourmetCoffeeShop()
+        {
+
+        }
         
+        public GourmetCoffeeShop(int iCode,String iName, Dictionary<int, double> iPrice):base(iCode,iName,iPrice)
+        {
+            ItemCode = iCode;
+            ItemName = iName;
+            ItemPrice = iPrice;
+        }
+
+        public override double PayPerPiece(int quantity)
+        {
+            //totalBillValue = ItemPrice * quantity;
+            return 1;
+        }
+
+        public override double PayPerWeight(int weight)
+        {
+            throw new NotImplementedException();
+        }
+
+        double ITaxCompute.TaxOnTotalBill()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
